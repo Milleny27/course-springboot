86d4b39e98adf058f79c5f86a19b5a503f155317
Milleny <ellenmilleny@gmail.com>
2022-07-09 15:18:07 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/portfolio/course/entities/Product.java
+++ src/main/java/com/portfolio/course/entities/Product.java
@@ -2,20 +2,20 @@ package com.portfolio.course.entities;
 
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -26,11 +26,11 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
-	@Transient
-	private Set<Category> categories = new HashSet();
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
-
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -82,17 +82,16 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
-	public void setCategories(Set<Category> categories) {
-		this.categories = categories;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -104,7 +103,11 @@ public class Product implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Product other = (Product) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
 }
